2daf2ab6de03f10ba300353396ac9bbbbca5079f
phanikumar1210 <phanikumardadi.kumar@gmail.com>
2018-11-16 06:10:35 +0530

    Update Sample.ps1

--- Sample.ps1
+++ Sample.ps1
@@ -1,3 +1,10 @@
+#Get-ChildItem | ?{ $_.PSIsContainer } | Select-Object Name
+
+#$arr = Get-ChildItem | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
+
+$items = Get-ChildItem
+#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
 foreach ($item in $items)
 {
       # if the item is a directory, then process it.
@@ -6,7 +13,9 @@ foreach ($item in $items)
             $count = (git log --name-status -1 $item.Name | select-string -pattern $args[0]).length
             if($count -gt 1 -or $count -eq 1)
             {
-                Write-Host $item.Name$count
+                New-Item -ItemType directory -Path Jenkins$item
+                Write-Host $item$count
             }
+            Write-Host $item$count
       }
 }
